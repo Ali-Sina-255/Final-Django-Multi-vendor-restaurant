02337925ccec42630581fd69634d17b902f1c4bb
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-08-03 15:46:47 +0430

    menu bulilder and food item CRUD is done

--- menu/forms.py
+++ menu/forms.py
@@ -40,3 +40,17 @@ class FoodItemForm(forms.ModelForm):
             "image",
             "is_available",
         ]
+
+    def __init__(self, *args, **kwargs):
+        super(FoodItemForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            # Apply Tailwind CSS classes to all fields except 'image'
+            if field != "image":
+                self.fields[field].widget.attrs.update(
+                    {
+                        "class": "appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    }
+                )
+
+        # Customize 'image' field separately
+        self.fields["image"].widget.attrs.update({"class": "btn btn-info w-100"})
